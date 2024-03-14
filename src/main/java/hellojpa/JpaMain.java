0045eefd7d95e8779d0863fb72cd1392722b5274
kwon <kckc0608@naver.com>
2024-03-14 22:30:14 +0900

    feat: jpa를 이용한 데이터 insert 코드
    JPA가 스프링과 관련된 기술이라고만 생각했었는데, 전혀 아니었다.
    JPA는 자바라는 언어와 관련된 기술!
    그래서 이 예제 코드에서는 스프링 프레임워크 설정이 전혀 없다..!

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,7 +8,18 @@ public class JpaMain {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
+
         //code
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setName("hello");
+
+        em.persist(member);
+        transaction.commit();
 
         em.close();
         emf.close();
